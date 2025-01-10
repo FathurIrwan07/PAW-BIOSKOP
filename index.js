4508b0dc22fd224a67795d9356929670774ad9a7
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2025-01-11 00:07:28 +0700

    perbaikan semua code

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// Definisi Library yang digunakan
+// Import Library yang digunakan
 const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const path = require("path");
-const flash = require("connect-flash"); // Memilih satu library flash
+const flash = require("connect-flash");
+
 const app = express();
 
 // Definisi lokasi file router
 const loginRoutes = require("./src/routes/router-login");
 const registerRoutes = require("./src/routes/router-register");
 const contactRoutes = require("./src/routes/router-contact");
-const todosRoutes = require("./src/routes/router-todo");
 const appRoutes = require("./src/routes/router-app");
 
 // Konfigurasi library session
@@ -27,13 +27,13 @@ app.use(
   })
 );
 
-// Configurasi dan gunakan library
+// Konfigurasi dan gunakan library
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(flash()); // Gunakan connect-flash untuk pesan flash
 
 // Set headers untuk menghindari caching
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.setHeader(
     "Cache-Control",
     "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
@@ -42,18 +42,27 @@ app.use(function (req, res, next) {
   next();
 });
 
-// Configurasi static folder untuk public assets
-app.use("/public", express.static(path.join(__dirname, "src/public")));
+// Konfigurasi static folder untuk public assets
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 // Setting folder views dan view engine
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
 
-// Gunakan routes yang telah didefinisikan
+// Rute untuk todos
+app.get("/todos", (req, res) => {
+  const url = "/"; // Atur URL dasar sesuai kebutuhan
+  res.render("todos", { url }); // Kirim url ke template todos.ejs
+});
+
+// Route for the team page
+app.get("/team", (req, res) => {
+  res.render("team", { url }); // Pastikan mengirimkan variabel 'url' ke file ejs
+});
+
 app.use("/login", loginRoutes);
 app.use("/register", registerRoutes);
 app.use("/contact", contactRoutes);
-app.use("/todos", todosRoutes);
 app.use("/", appRoutes);
 
 // Middleware untuk penanganan error
@@ -63,6 +72,7 @@ app.use((err, req, res, next) => {
 });
 
 // Gunakan port server
-app.listen(5050, () => {
-  console.log("Server Berjalan di Port : " + 5050);
+const PORT = process.env.PORT || 5051; // Menggunakan PORT dari environment atau default ke 5050
+app.listen(PORT, () => {
+  console.log(`Server Berjalan di Port: ${PORT}`);
 });
