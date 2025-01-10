8faa255bf6f33f0148b899a19cfd497b539a04f9
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2025-01-11 01:32:29 +0700

    Perbaiki Port

--- src/controllers/controller-register.js
+++ src/controllers/controller-register.js
@@ -1,38 +1,40 @@
-const config = require('../configs/database');
-let mysql      = require('mysql');
-let pool       = mysql.createPool(config);
+const config = require("../configs/database");
+let mysql = require("mysql");
+let pool = mysql.createPool(config);
 
-pool.on('error',(err)=> {
-    console.error(err);
+pool.on("error", (err) => {
+  console.error(err);
 });
 
-module.exports ={
-    formRegister(req,res){
-        res.render("register",{
-            url : 'http://localhost:5050/',
-        });
-    },
-    saveRegister(req,res){
-        let username = req.body.username;
-        let email = req.body.email;
-        let password = req.body.pass;
-        if (username && email && password) {
-            pool.getConnection(function(err, connection) {
-                if (err) throw err;
-                connection.query(
-                    `INSERT INTO users (nama,email,password) VALUES (?,?,SHA2(?,512));`
-                , [username, email, password],function (error, results) {
-                    if (error) throw error; 
-                    req.flash('color', 'success');
-                    req.flash('status', 'Yes..');
-                    req.flash('message', 'Registrasi berhasil');
-                    res.redirect('/login');
-                });
-                connection.release();
-            })
-        } else {
-            res.redirect('/login');
-            res.end();
-        }
+module.exports = {
+  formRegister(req, res) {
+    res.render("register", {
+      url: "http://localhost:5050/",
+    });
+  },
+  saveRegister(req, res) {
+    let username = req.body.username;
+    let email = req.body.email;
+    let password = req.body.pass;
+    if (username && email && password) {
+      pool.getConnection(function (err, connection) {
+        if (err) throw err;
+        connection.query(
+          `INSERT INTO users (nama,email,password) VALUES (?,?,SHA2(?,512));`,
+          [username, email, password],
+          function (error, results) {
+            if (error) throw error;
+            req.flash("color", "success");
+            req.flash("status", "Yes..");
+            req.flash("message", "Registrasi berhasil");
+            res.redirect("/login");
+          }
+        );
+        connection.release();
+      });
+    } else {
+      res.redirect("/login");
+      res.end();
     }
-}
+  },
+};
